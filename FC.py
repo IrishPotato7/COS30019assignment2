5af72dd16baa40e6ecb6f7ee4cd0be6c3408eadb
amoolie <amelietlx@gmail.com>
2023-05-21 18:58:58 +1000

    FC functions correctly
    added testcases

--- FC.py
+++ FC.py
@@ -1,33 +1,49 @@
+#The new FC
+
+
 import re
 import KB
 
 #Add values to the queue list based on the sentences that are known to be true
 def findRules(kb):
+    rule =[]
     queue =[]
+    checkedTrue=[]
     #Add first rules (eg. a, b, p2)
     for argument in kb:
         argumentparams = re.split("&|=>", argument)  
         if argumentparams[0]==argumentparams[-1]:
-            queue.append(argumentparams[0])
-    return queue
+            rule.append(argumentparams[0])
+            checkedTrue.append(argumentparams[0])
+    return rule, queue, checkedTrue
 
 #If LHS of sentence is in queue list, add RHS of sentence to queue
-def TruthValue(queue, kb, query):
-    if query in kb:
-        return [query]
+def TruthValue(checkedTrue, queue, kb, query):
+    
+    if query in checkedTrue:
+        return queue
     for argument in kb:
         argumentparams = re.split("&|=>", argument)
-        if query in queue:
+        print("Queue",queue)
+        if queue is False:
+            return
+        elif query in queue:
             return queue
-        elif argumentparams[0] in queue and argumentparams[-2] in queue and argumentparams[-1] not in queue:
-            queue.append(argumentparams[-1])
-            queue = TruthValue(queue, kb, query)
-    return False
+        try:
+            if argumentparams[0] in checkedTrue and argumentparams[-2] in checkedTrue and argumentparams[-1] not in queue:
+                queue.append(argumentparams[-1])
+                checkedTrue.append(argumentparams[-1])
+                queue = TruthValue(checkedTrue, queue, kb, query)
+                continue
+        except:
+            queue=[]
+            return False
 
 def askFC(kb, query):
-    values = findRules(kb)
-    result = TruthValue(values, kb, query)
-    if result:
+    rule, queue, checkedTrue = findRules(kb)
+    result = TruthValue(checkedTrue, queue, kb, query)
+    if result is not False :
+        result = rule + result
         result = ", ".join(result)
         print(f"YES: {result}")
     else:
