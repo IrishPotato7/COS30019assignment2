5df731d2fe6c5e9e9863e2bfdb63491fdc45a06b
amoolie <amelietlx@gmail.com>
2023-05-21 19:17:02 +1000

    Changed function names

--- FC.py
+++ FC.py
@@ -5,7 +5,7 @@ import re
 import KB
 
 #Add values to the queue list based on the sentences that are known to be true
-def findRules(kb):
+def FirstRules(kb):
     rule =[]
     queue =[]
     checkedTrue=[]
@@ -18,8 +18,7 @@ def findRules(kb):
     return rule, queue, checkedTrue
 
 #If LHS of sentence is in queue list, add RHS of sentence to queue
-def TruthValue(checkedTrue, queue, kb, query):
-    
+def findQueue(checkedTrue, queue, kb, query):
     if query in checkedTrue:
         return queue
     for argument in kb:
@@ -32,15 +31,15 @@ def TruthValue(checkedTrue, queue, kb, query):
             if argumentparams[0] in checkedTrue and argumentparams[-2] in checkedTrue and argumentparams[-1] not in queue:
                 queue.append(argumentparams[-1])
                 checkedTrue.append(argumentparams[-1])
-                queue = TruthValue(checkedTrue, queue, kb, query)
+                queue = findQueue(checkedTrue, queue, kb, query)
                 continue
         except:
             queue=[]
             return False
 
 def askFC(kb, query):
-    rule, queue, checkedTrue = findRules(kb)
-    result = TruthValue(checkedTrue, queue, kb, query)
+    rule, queue, checkedTrue = FirstRules(kb)
+    result = findQueue(checkedTrue, queue, kb, query)
     if result is not False :
         result = rule + result
         result = ", ".join(result)
