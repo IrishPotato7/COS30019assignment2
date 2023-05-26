e5ff89d374ab3870d7fb7ee113ac9a0b24ea3216
Alex Ireland <77884864+IrishPotato7@users.noreply.github.com>
2023-05-26 11:14:02 +1000

    BC comments

--- BC.py
+++ BC.py
@@ -1,12 +1,18 @@
 import re
 
+# Asks if a sentence is true by asking if the sentences that infer it are true
 def TruthValue(kb, query):
+    # Returns the query if it is true in the kb
     if query in kb:
         return [query]
+
+    # Checks if the query is inferred in the argument
     for argument in kb:
         argumentparams = re.split("&|=>", argument)
         if query != argumentparams[-1]:
             continue
+
+        # Runs this function for the statements in the argument
         values =[]
         for statement in argumentparams[:-1]:
             value = TruthValue(kb, statement)
@@ -17,9 +23,10 @@ def TruthValue(kb, query):
             else:
                 values = value + values
         values.append(query)
-        return values      
+        return values
     return False
 
+# Initiates the recursive TruthValue function
 def askBC(kb, query):
     result = TruthValue(kb, query)
     if result:
